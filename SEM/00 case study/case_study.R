78667cdac65c43a7d0d3a605d40a52f0bbe2c15d
chris-allones <62604005@kmitl.ac.th>
2021-06-24 16:16:16 +0700

    case_study

---
+++ SEM/00 case study/case_study.R
@@ -0,0 +1,12 @@
+
+
+
+# Library
+library(tidyverse)
+library(readxl)
+library(janitor)
+
+
+# Loading case data ----
+
+case_data <- read_excel("00 case study/e_bus_customer_satisfaction.xlsx") %>% clean_names()
