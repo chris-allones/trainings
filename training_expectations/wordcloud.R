b7f93c1f1ba7a61b171fa2891f3c0333f87c2de1
chris-allones <christopher.allones@gmail.com>
2022-01-03 11:00:00 +0700

    wordcloud expectations

---
+++ training_expectations/wordcloud.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(readxl)
+library(tidytext)
+library(janitor)
+library(ggwordcloud)
+
+# import data
+data_expectations <- read_excel("participants-profile.xlsx") %>% 
+ clean_names() %>%
+ select(expectations = starts_with("expectations")) %>% 
+ cbind(id = 1:nrow(.)) %>% 
+ as_tibble() %>% 
+ filter(expectations!= ".")
+
+#tidytext
+
+tokenized_expectations <- unnest_tokens(data_expectations, 
+                                        input = expectations,
+                                        output = word)
+
+tidy_expecatations <- tokenized_expectations %>% 
+ anti_join(stop_words) %>% 
+ count(word, sort = TRUE) %>% 
+ rename(count = n)
+
+
+ggwordcloud_expectations <- tidy_expecatations %>% 
+ filter(count > 2) %>% 
+ ggplot(aes(label = word, size = count, color = count)) +
+ geom_text_wordcloud() + 
+ scale_size_area(max_size = 27) +
+ theme_light() +
+ theme(panel.border = element_blank())
+
+ggsave('ggwordcloud_expectations.jpg')
